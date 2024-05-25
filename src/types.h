535c6d62a7be179af11e12e11b3d5251ff389196
Maurice <maur.ice.weber@icloud.com>
2024-05-25 20:31:27 +0200

    change include in types.h to Eigen/dense - expecting to fail

--- src/types.h
+++ src/types.h
@@ -1,8 +1,8 @@
 #ifndef __TYPES_H
 #define __TYPES_H
 
-// #include <Eigen/Dense>
-#include <eigen-3.4.0/Eigen/Dense>
+#include <Eigen/Dense>
+// #include <eigen-3.4.0/Eigen/Dense>
 
 using Positions_t = Eigen::Array3Xd;
 using Velocities_t = Eigen::Array3Xd;
